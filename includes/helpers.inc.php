28529b42810420869aa45bdc5944904d9609a356
Matthews offen <matthewsoffen2@gmail.com>
2024-12-24 23:41:26 +0200

    Update helpers.inc.php [ correct verification function

--- includes/helpers.inc.php
+++ includes/helpers.inc.php
@@ -22,8 +22,7 @@ function sanitize($text){
 */
 function report_error($error){
 	$error_report = $error->getMessage();
-	echo $error->getMessage();
-	header('Location: errors/?error="'.$error_report.'"');
+	header('Location: errors/?error="'."$error_report".'"');
 	exit();
 }
 
@@ -33,12 +32,71 @@ function report_error($error){
 */
 function connecttodb($host, $database, $user, $password){
 	try{	
-		$con = new PDO('mysqlhost='.$host.';dbname='.$database, $user, $password);
+		$con = new PDO('mysql:host='.$host.';dbname='.$database, $user, $password);
 		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$con->exec('SET NAMES "utf8"');
+		return $con;
+	}catch(PDOException $e){
+		echo $e->getMessage();
+		report_error($e);	
+	}
+}
+
+//verifies credentials
+//tries to avoid timing attacks
+function authorise($con, $name, $password) {
+	try{		
+		$sql = 'SELECT name, password FROM clients WHERE name="'.$name.'"
+				 AND password="'.md5($name.$password).'"';
+		$count = 0;
+		$result = $con->query($sql);
+		$row = $result->fetch();		
+		if($row != false && 
+			$name == $row['name'] && 
+			$row['password'] == md5($name.$password)) {
+			return true;
+		}
+		else{
+			echo "false : ".md5($name.$password); 
+			return false;
+		}
 	}catch(PDOException $e){
 		
-		echo $e->getMessage();
-		//report_error($e);	
-	}
+	}	
+}
+
+/*
+* redirects a user to a specified page based on the user's
+* group.
+* if it fails to do so because of database issues or something,
+* user is sent back to the login page.
+*/
+function redirect_user($con, $name, $password) {
+	try{
+		//fetch user's group
+		$sql = 'SELECT groupId FROM clients WHERE name="'.$name.'"
+				 AND password="'.md5($name.$password).'"';
+		$result = $con->query($sql);
+		foreach($result as $record){
+			$groupId = $record["groupId"] == null ? null : $record["groupId"]; 		
+		}
+		
+		//redirect 
+		switch($groupId) {
+			case "admin" : {
+				header('Location: admin/');
+				break;				
+			}
+			case "cashier" : {
+				header('Location: sales/');
+				break;
+			}
+			default:{
+				header('Location: ../pos_web');
+				break;				
+			}
+		}			
+	}catch(PDOException $e){
+		report_error($e);
+	}	
 }
