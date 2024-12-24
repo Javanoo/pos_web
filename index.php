173a1c8a717b0f1b4e4eccfbd5d8f05db80f9370
Matthews offen <matthewsoffen2@gmail.com>
2024-12-24 23:42:14 +0200

    Update index.php [ match updated correct verification function]

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	//include files
 	require 'includes/helpers.inc.php';
-	$con = connecttodb("localhost", "pos_shop", "root", "roots.5");
+	$con = connecttodb("localhost", "pos_shop", "root", "");
 	
 	//get credentials
 	$name = isset($_POST["name"]) ? sanitize($_POST["name"]) : null;
@@ -30,52 +30,4 @@
 		include 'login.html';
 		exit();	
 	}
-	
-	/*
-	* redirects a user to a specified page based on the user's
-	* group.
-	* if it fails to do so because of database issues or something,
-	* user is sent back to the login page.
-	*/
-	function redirect_user($con, $name, $password) {
-		try{
-			//fetch user's group
-			$sql = 'SELECT groupId FROM users WHERE name="'.$name.'"
-					 AND password="'.md5($name.$password).'"';
-			$result = $con->exec($sql);
-			foreach($result as $record){
-				$groupId = $record["groupId"] == null ? null : $record["groupId"]; 		
-			}
-			
-			//redirect 
-			switch($groupId) {
-				case "admin" : {
-					header('Location: admin/');
-					break;				
-				}
-				case "cashier" : {
-					header('Location: sales/');
-					break;
-				}
-				default:{
-					header('Location: ../pos_web');
-					break;				
-				}
-			}			
-		}catch(PDOException $e){
-			//report_error($e);
-		}	
-	}
-	
-	//verifies credentials
-	//tries to avoid timing attacks
-	function authorise($con, $name, $password) {
-		$sql = 'SELECT COUNT(*) FROM users WHERE name="'.$name.'"
-				 AND password="'.md5($name.$password).'"';
-		$count = 0;
-		$count = $con->exec($sql);
-		if($count == 1) return true;
-		else return false;	
-	}
-	
 ?>
