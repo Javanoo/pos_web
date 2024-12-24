3a04d81bbf1cde9f02c3a8b1ff5f516d78481d31
Matthews offen <matthewsoffen2@gmail.com>
2024-12-24 23:43:55 +0200

    initial controller file for the sales templates

--- sales/index.php
+++ sales/index.php
@@ -1,3 +1,15 @@
 <?php
-
+	$total = 0.0;
+	$paid = 0.0;
+	$change = 0.0;
+	
+	if(isset($_GET["history"])) {
+		include 'history';
+		exit;	
+	}elseif(isset($_GET["logout"]) &&
+				$_GET["logout"] == 'on') {
+	}else {
+		include 'sales.html';
+		exit();	
+	}
 ?>
