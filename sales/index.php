1e0ac9a73e807fb61b231d8bdb30235c8b69d5c0
Matthews offen <matthewsoffen2@gmail.com>
2025-01-12 08:57:49 +0200

    update [remove unnecessary switches]

--- sales/index.php
+++ sales/index.php
@@ -3,13 +3,6 @@
 	$paid = 0.0;
 	$change = 0.0;
 	
-	if(isset($_GET["history"])) {
-		include 'history';
-		exit;	
-	}elseif(isset($_GET["logout"]) &&
-				$_GET["logout"] == 'on') {
-	}else {
-		include 'sales.html';
-		exit();	
-	}
+	include 'sales.html';
+	exit();
 ?>
